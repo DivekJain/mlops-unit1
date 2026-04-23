00ba91e3da738b305255fd1d29a1fdf69119b2e1
DivekJain <divekjain10@gmail.com>
2026-04-23 16:08:33 +0530

    Update train_cancer.py

--- experiment6/train_cancer.py
+++ experiment6/train_cancer.py
@@ -6,9 +6,11 @@ from sklearn.metrics import accuracy_score
 data = load_breast_cancer()
 X, y = data.data, data.target
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
-model = RandomForestClassifier()
+model = RandomForestClassifier(random_state=42)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
